666d975a29540fbd5b93309003fbcabba07b51eb
Suren081303 <suren.gevorgyan.2003@gmail.com>
2026-07-13 19:52:37 +0400

    Revert "Architectural fixing"
    This reverts commit 5baa49e299814249613f625b1b3c606ae391e10d.

---
+++ Models/DetectRequest.cs
@@ -0,0 +1,7 @@
+namespace DetectApi.Models
+{
+    public class DetectRequest
+    {
+        public string Value { get; set; } = string.Empty;
+    }
+}
---
+++ Models/DetectResponse.cs
@@ -0,0 +1,8 @@
+namespace DetectApi.Models
+{
+    public class DetectResponse
+    {
+        public string Type { get; set; } = string.Empty;
+        public string? Provider { get; set; }
+    }
+}
